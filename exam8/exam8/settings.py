5f0ab32a7f5b2c7865ece74dcf516803f0204eb8
Maria Sharshenova <maria@sharshenov.com>
2019-03-08 20:04:12 +0600

    Подключен в настройки 'rest_framework'

--- exam8/exam8/settings.py
+++ exam8/exam8/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'webapp',
     'api_v1'
 ]
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
